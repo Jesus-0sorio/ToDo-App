40c29680b29b2cf9fc4b10367e5cfbdd2247d038
Jesus-0sorio <jaoz10.16@gmail.com>
2023-09-20 12:42:53 -0500

    fix: remove MySQL URI

--- ToDo-API/src/db/db.js
+++ ToDo-API/src/db/db.js
@@ -1,8 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  'mysql://root:tW9pK96r5iuxbx1N2Zm6@containers-us-west-136.railway.app:6597/railway'
-);
+const sequelize = new Sequelize();
 
 
 module.exports = sequelize;
